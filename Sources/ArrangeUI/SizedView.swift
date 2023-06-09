bbfd7fd059c66b07a75c169e5c83695163830f74
Daniel Inoa <danielinoa@gmail.com>
2023-06-09 01:02:10 -0700

    Allow for unspecified dimensions

--- Sources/ArrangeUI/SizedView.swift
+++ Sources/ArrangeUI/SizedView.swift
@@ -5,18 +5,19 @@
 import UIKit
 
 /// A SizedView sizes the wrapped-view to the specified size without considering any size proposal.
+/// Unspecified dimensions will adopt the proposal or the underlying view's intrinsic size dimension.
 /// This is different to FrameView, where the wrapped-view is proposed a size and it determines the size that fits best.
 public class SizedView: UIView {
 
     public let view: UIView
 
-    public let width: Double
+    public let width: Double?
 
-    public let height: Double
+    public let height: Double?
 
     // MARK: - Lifecycle
 
-    public init(_ view: UIView, width: Double, height: Double) {
+    public init(_ view: UIView, width: Double?, height: Double?) {
         self.view = view
         self.width = width
         self.height = height
@@ -31,16 +32,25 @@ public class SizedView: UIView {
     // MARK: - Layout
 
     public override var intrinsicContentSize: CGSize {
-        .init(width: width, height: height)
+        .init(
+            width: width ?? view.intrinsicContentSize.width,
+            height: height ?? view.intrinsicContentSize.height
+        )
     }
 
     public override func sizeThatFits(_ proposal: CGSize) -> CGSize {
-        .init(width: width, height: height)
+        .init(
+            width: width ?? proposal.width,
+            height: height ?? proposal.height
+        )
     }
 
     public override func layoutSubviews() {
         super.layoutSubviews()
-        view.frame.size = .init(width: width, height: height)
+        view.frame.size = .init(
+            width: width ?? bounds.width,
+            height: height ?? bounds.height
+        )
         view.frame.centerX = bounds.centerX
         view.frame.centerY = bounds.centerY
     }
@@ -48,7 +58,7 @@ public class SizedView: UIView {
 
 public extension UIView {
 
-    func sized(width: Double, height: Double) -> SizedView {
+    func sized(width: Double? = nil, height: Double? = nil) -> SizedView {
         .init(self, width: width, height: height)
     }
 
