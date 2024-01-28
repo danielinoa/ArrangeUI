7eaa1363b99c83479580e6f3c799104c3f0d89fc
Daniel Inoa <danielinoa@gmail.com>
2024-01-27 18:52:35 -0800

    Remove arrangedSubviews property

--- Sources/ArrangeUI/Stacks/VStackView.swift
+++ Sources/ArrangeUI/Stacks/VStackView.swift
@@ -3,48 +3,25 @@
 //
 
 import UIKit
-import Collections
 import Rectangular
 
 /// A view that arranges its subviews along the horizontal axis.
 public class VStackView: UIView {
 
     public var alignment: HorizontalAlignment {
-        get {
-            layout.alignment
-        }
+        get { layout.alignment }
         set {
-            guard layout.alignment != newValue else { return }
             layout.alignment = newValue
-            setNeedsArrangement()
+            setAncestorsNeedLayout()
         }
     }
 
     /// The vertical distance between adjacent items within the stack.
     public var spacing: Double {
-        get {
-            layout.spacing
-        }
+        get { layout.spacing }
         set {
-            guard layout.spacing != newValue else { return }
             layout.spacing = newValue
-            setNeedsArrangement()
-        }
-    }
-
-    /// The list of views arranged by the stack view.
-    public var arrangedSubviews: [UIView] = [] {
-        didSet {
-            let diff = (arrangedSubviews as [UIView]).difference(from: oldValue)
-            diff.forEach { change in
-                switch change {
-                case .insert(let offset, let element, _):
-                    insertSubview(element, at: offset)
-                case .remove(_, let element, _):
-                    element.removeFromSuperview()
-                }
-            }
-            setNeedsArrangement()
+            setAncestorsNeedLayout()
         }
     }
 
@@ -57,34 +34,25 @@ public class VStackView: UIView {
         super.init(frame: .zero)
     }
 
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - Hierarchy
-
-    @discardableResult
-    public func callAsFunction(@ViewBuilder _ content: () -> ViewBuilder.Composite) -> Self {
-        arrangedSubviews = content().items().compactCast()
-        return self
     }
 
     // MARK: - Layout
 
     public override var intrinsicContentSize: CGSize {
-        layout.sizeThatFits(items: arrangedSubviews).asCGSize
+        layout.sizeThatFits(items: subviews).asCGSize
     }
 
     public override func sizeThatFits(_ proposedSize: ProposedSize) -> PreferredSize {
-        let size = layout.sizeThatFits(items: arrangedSubviews, within: proposedSize.asSize).asCGSize
-        return size
+        layout.sizeThatFits(items: subviews, within: proposedSize.asSize).asCGSize
     }
 
     public override func layoutSubviews() {
         super.layoutSubviews()
-        guard arrangedSubviews.hasElements else { return }
-        let frames = layout.frames(for: arrangedSubviews, within: bounds.asRect).map(\.asCGRect)
-        zip(arrangedSubviews, frames).forEach { view, frame in
+        guard subviews.hasElements else { return }
+        let frames = layout.frames(for: subviews, within: bounds.asRect).map(\.asCGRect)
+        zip(subviews, frames).forEach { view, frame in
             view.frame = frame
         }
     }
