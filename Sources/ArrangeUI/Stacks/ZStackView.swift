4d95d702d3207e1989b353a396ebb4be79421a7a
Daniel Inoa <danielinoa@gmail.com>
2024-01-27 18:52:03 -0800

    Use ZStackLayout for ZStackView

--- Sources/ArrangeUI/Stacks/ZStackView.swift
+++ Sources/ArrangeUI/Stacks/ZStackView.swift
@@ -8,32 +8,19 @@ import Rectangular
 public class ZStackView: UIView {
 
     public var alignment: Alignment {
-        didSet {
-            guard alignment != oldValue else { return }
-            setNeedsArrangement()
+        get { layout.alignment }
+        set {
+            layout.alignment = newValue
+            setAncestorsNeedLayout()
         }
     }
 
-    /// The list of views arranged by the stack view.
-    public var arrangedSubviews: [UIView] = [] {
-        didSet {
-            let diff = (arrangedSubviews as [UIView]).difference(from: oldValue)
-            diff.forEach { change in
-                switch change {
-                case .insert(let offset, let element, _):
-                    insertSubview(element, at: offset)
-                case .remove(_, let element, _):
-                    element.removeFromSuperview()
-                }
-            }
-            setNeedsArrangement()
-        }
-    }
+    private var layout: ZStackLayout
 
     // MARK: - Lifecycle
 
     public init(alignment: Alignment = .center) {
-        self.alignment = alignment
+        self.layout = .init(alignment: alignment)
         super.init(frame: .zero)
     }
 
@@ -41,67 +28,22 @@ public class ZStackView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // MARK: - Hierarchy
-
-    @discardableResult
-    public func callAsFunction(@ViewBuilder _ content: () -> ViewBuilder.Composite) -> Self {
-        arrangedSubviews = content().items().compactCast()
-        return self
-    }
-
     // MARK: - Layout
 
     public override var intrinsicContentSize: CGSize {
-        let maxWidth = arrangedSubviews.map(\.intrinsicContentSize.width).max() ?? .zero
-        let maxHeight = arrangedSubviews.map(\.intrinsicContentSize.height).max() ?? .zero
-        return .init(width: maxWidth, height: maxHeight)
+        layout.sizeThatFits(items: subviews).asCGSize
     }
 
     public override func sizeThatFits(_ proposedSize: ProposedSize) -> PreferredSize {
-        let fittingSizes = arrangedSubviews.map { $0.sizeThatFits(proposedSize) }
-        let maxFittingWidth = fittingSizes.map(\.width).max() ?? .zero
-        let maxFittingHeight = fittingSizes.map(\.height).max() ?? .zero
-        let size = CGSize(width: maxFittingWidth, height: maxFittingHeight)
-        return size
+        layout.sizeThatFits(items: subviews, within: proposedSize.asSize).asCGSize
     }
 
     public override func layoutSubviews() {
         super.layoutSubviews()
-        for view in arrangedSubviews {
-            let fittingSize = view.sizeThatFits(bounds.size)
-            view.frame.size = fittingSize
-            switch alignment {
-            case .topLeading:
-                view.frame.leadingX = bounds.leadingX
-                view.frame.topY = bounds.topY
-            case .top:
-                view.frame.centerX = bounds.centerX
-                view.frame.topY = bounds.topY
-            case .topTrailing:
-                view.frame.trailingX = bounds.trailingX
-                view.frame.topY = bounds.topY
-            case .leading:
-                view.frame.leadingX = bounds.leadingX
-                view.frame.centerY = bounds.centerY
-            case .center:
-                view.frame.centerX = bounds.centerX
-                view.frame.centerY = bounds.centerY
-            case .trailing:
-                view.frame.trailingX = bounds.trailingX
-                view.frame.centerY = bounds.centerY
-            case .bottomLeading:
-                view.frame.leadingX = bounds.leadingX
-                view.frame.bottomY = bounds.bottomY
-            case .bottom:
-                view.frame.centerX = bounds.centerX
-                view.frame.bottomY = bounds.bottomY
-            case .bottomTrailing:
-                view.frame.trailingX = bounds.trailingX
-                view.frame.bottomY = bounds.bottomY
-            default:
-                view.frame.centerX = bounds.centerX
-                view.frame.centerY = bounds.centerY
-            }
+        guard subviews.hasElements else { return }
+        let frames = layout.frames(for: subviews, within: bounds.asRect).map(\.asCGRect)
+        zip(subviews, frames).forEach { view, frame in
+            view.frame = frame
         }
     }
 }
