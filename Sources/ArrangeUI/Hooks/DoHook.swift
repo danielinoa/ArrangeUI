6fa78d4a8a5a36dd2a73d88b6b13cbc5364c884d
Daniel Inoa <danielinoa@gmail.com>
2024-07-15 23:55:50 -0700

    Add DoHook

---
+++ Sources/ArrangeUI/Hooks/DoHook.swift
@@ -0,0 +1,27 @@
+//
+//  Created by Daniel Inoa on 7/15/24.
+//
+
+import UIKit
+
+final class DoHook<T>: Hook {
+
+    let forward: (T) -> Void
+
+    init(_ closure: @escaping (T) -> Void) {
+        self.forward = closure
+    }
+
+    func viewWillBeAddedToArrangedTree(_ view: UIView) {
+        guard let castedView = view as? T else { return }
+        forward(castedView)
+    }
+}
+
+public extension Arranged {
+
+    func `do`<T>(_ closure: @escaping (T) -> Void) -> Arranged {
+        let hook = DoHook<T>(closure)
+        return HookHost.init(arrangedContent: self, hook: hook)
+    }
+}
